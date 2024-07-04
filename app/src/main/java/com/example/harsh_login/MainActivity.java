451688c95ff6890e4c69b61938f87fa9b3c56c11
iamkp19 <iamkp19.19@gmail.com>
2024-07-04 19:06:28 +0530

    add another page

--- app/src/main/java/com/example/harsh_login/MainActivity.java
+++ app/src/main/java/com/example/harsh_login/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.harsh_login;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,24 +16,27 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("MissingInflatedId")
-    @Override
+
     Button login;
 
-    login =
-
     @Override
-    public <T extends View> T findViewById(int id) {
-        return super.findViewById(id);
-    }
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        View button = findViewById(R.id.button_);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(),"Sign up sucessfully",Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);;
+                startActivity(intent);
+            }
         });
+
+        }
     }
-}
