d109beeb23194709318dd9de27dbe42e239f8490
iamkp19 <iamkp19.19@gmail.com>
2024-07-06 00:32:13 +0530

    Email Id & Password Validation required

--- app/src/main/java/com/example/harsh_login/MainActivity.java
+++ app/src/main/java/com/example/harsh_login/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,11 +14,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("MissingInflatedId")
 
     Button login;
+    EditText email, password;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
 
@@ -26,15 +31,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         View button = findViewById(R.id.button_);
+         email = findViewById(R.id.main_emial);
+         password = findViewById(R.id.main_password);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getApplicationContext(),"Sign up sucessfully",Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);;
-                startActivity(intent);
+                if (email.getText().toString().trim().equals("")) {
+                    email.setError("Email Id Required !");
+                }
+                else if (email.getText().toString().trim().matches(emailPattern)) {
+                    email.setError("Valid Email Id Required !");
+                    
+                }
+                else if (password.getText().toString().trim().equals("")) {
+                    password.setError("Password Required !");
+                }
+                else if (password.getText().toString().trim().length()>6) {
+                    password.setError("Minimum 6 char Required !");
+
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Sign up sucessfully", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(view, "You Are Logged In", Snackbar.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                    ;
+                    startActivity(intent);
+                }
+
             }
         });
 
